db61a6891065954a8ed4954c61b33c131a9ede13
brentlavelle@yahoo.com <brentlavelle@yahoo.com>
2011-03-02 01:37:54 -0600

    Converting to webdriver

--- koans/about_getting_started.rb
+++ koans/about_getting_started.rb
@@ -1,6 +1,13 @@
 #!/usr/bin/env ruby
 # -*- ruby -*-
+require 'rubygems'
+require 'watir-webdriver'
 
-require "watir"
-b = Watir::Browser.new
-b.goto File.expand_path(File.dirname(__FILE__) + '/html/simple.html')
+html_dir = File.expand_path(File.dirname(__FILE__) + '/html')
+simple_html = "FILE://#{html_dir}/simple.html"
+puts "Simple is '#{simple_html}'"
+
+b = Watir::Browser.new(:firefox)
+b.goto simple_html
+puts "Text on simple page is should be 'Simple HTML content' is: '#{b.text}'"
+
