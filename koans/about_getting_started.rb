fc1c9af8ac18fbc6837277f819daf234c4726287
brentlavelle@yahoo.com <brentlavelle@yahoo.com>
2011-03-02 23:16:28 -0600

    Start using rspec for testing assertions

--- koans/about_getting_started.rb
+++ koans/about_getting_started.rb
@@ -1,13 +1,27 @@
 #!/usr/bin/env ruby
 # -*- ruby -*-
 require 'rubygems'
+require 'rspec'
 require 'watir-webdriver'
 
-html_dir = File.expand_path(File.dirname(__FILE__) + '/html')
-simple_html = "FILE://#{html_dir}/simple.html"
-puts "Simple is '#{simple_html}'"
 
-b = Watir::Browser.new(:firefox)
-b.goto simple_html
-puts "Text on simple page is should be 'Simple HTML content' is: '#{b.text}'"
+describe "Simple navigation and content checking" do
+  before(:all) do
+    html_dir = File.expand_path(File.dirname(__FILE__) + '/html')
+    simple_html = "FILE://#{html_dir}/simple.html"
+    $b = Watir::Browser.new(:firefox)
+    $b.goto simple_html
+  end
 
+  after(:all) do
+    $b.close
+  end
+
+  it "verify contents of simple html" do
+    $b.text.should == "Simple HTML content"
+  end
+
+  it "does not contain any complex HTML" do
+    $b.text.should_not match /complex +HTML/i
+  end
+end
