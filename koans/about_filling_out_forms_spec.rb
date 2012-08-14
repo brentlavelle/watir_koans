8ae15438aeece7de7a22e06921ed81f9d29cea9a
brentlavelle@yahoo.com <foobar>
2012-08-14 15:02:06 -0500

    Rubymine doesn't like one char instance vars and they are not a good idea here

--- koans/about_filling_out_forms_spec.rb
+++ koans/about_filling_out_forms_spec.rb
@@ -8,42 +8,42 @@ describe "Simple navigation and content checking" do
   before(:all) do
     html_dir = File.expand_path(File.dirname(__FILE__) + '/html')
     simple_html = "FILE://#{html_dir}/more.html"
-    @b = Watir::Browser.new(:firefox)
-    @b.goto simple_html
-    @f = @b.form(:id => 'newuser')
+    @browser = Watir::Browser.new(:firefox)
+    @browser.goto simple_html
+    @form = @browser.form(:id => 'newuser')
   end
 
   after(:all) do
-    @b.close
+    @browser.close
   end
 
   #Fixme forms should come later
   it "should have one form on this page" do
-    @b.forms.size.should == 1
+    @browser.forms.size.should == 1
   end
 
   it "should find the form by the id" do
-    @f.should exist
+    @form.should exist
   end
 
   it "should have a first name field by id with a name of firstname" do
-    @f.text_field(:id => "fname").name.should == "firstname"
+    @form.text_field(:id => "fname").name.should == "firstname"
   end
 
   it "should have a first name field by name" do
-    @f.text_field(:name => "firstname").id.should == "fname"
+    @form.text_field(:name => "firstname").id.should == "fname"
   end
 
   it "should flash 10 times and return the number of flashes" do
     # Flashing in the browser can help you identify page elements sometimes \
     # the flashing is not where we expect to see it.
-    @f.text_field(:id => "fname").flash.should == 10
+    @form.text_field(:id => "fname").flash.should == 10
   end
 
   it "has an HTML method for the view source crowd" do
     # Looking at the HTML, especially in irb, can help demystify a test, however it created from the DOM and
     # will probably be different that the document served
-    html_text = @f.text_field(:id => "fname").html
+    html_text = @form.text_field(:id => "fname").html
     html_text.should match(/^\<input /)
     html_text.should match(/\btype="text"/)
     html_text.should match(/\bname="firstname"/)
